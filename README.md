e63828abcbfa480adbf8a2063886b5194fab2371
zce <w@zce.me>
2020-08-16 21:09:25 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# caz-server
+
+> API Server for [CAZ](https://github.com/zce/caz)
+
+## Endpoints
+
+### GET `/api`
+
+Get user's repositories.
+
+```shell
+$ curl http://caz.vercel.app/api
+```
+
+Response Type:
+
+```typescript
+interface Item {
+  name: string
+  owner: string
+  fullName: string
+  description: string
+  updated: string
+}
+
+type Result = Item[]
+```
+
+## License
+
+[MIT](LICENSE) &copy; [zce](https://zce.me)
---
+++ api/index.ts
@@ -0,0 +1,42 @@
+import got from 'got'
+import { NowRequest, NowResponse } from '@vercel/node'
+
+interface Repository {
+  name: string
+  full_name: string
+  description: string
+  updated_at: string
+  owner: { login: string }
+}
+
+interface Item {
+  name: string
+  owner: string
+  fullName: string
+  description: string
+  updated: string
+}
+
+const fetchAllRepos = (owner: string) => {
+  return got.paginate<Repository>(`https://api.github.com/users/${owner}/repos?type=owner&sort=updated&per_page=100`, {
+    timeout: 5 * 1000,
+    username: process.env.GITHUB_CLIENT_ID!,
+    password: process.env.GITHUB_CLIENT_SECRET!
+  })
+}
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const owner = req.query.username ?? req.query.owner ?? 'caz-templates'
+  const items = fetchAllRepos(owner.toString())
+  const results: Item[] = []
+  for await (const item of items) {
+    results.push({
+      name: item.name,
+      owner: item.owner.login,
+      fullName: item.full_name,
+      description: item.description,
+      updated: item.updated_at
+    })
+  }
+  res.json(results)
+}
